4543c7681e2d46c89e630414fee9c79f9286adce
Esteban Espinoza Fallas <31984922+retornoaragond@users.noreply.github.com>
2024-11-18 16:57:37 -0600

    dotenv library added

--- src/api/universities.ts
+++ src/api/universities.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { UniversitiesResponse } from '../types/university';
 
-const API_BASE_URL = 'http://localhost:3000/api/v1';
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/v1';
 
 export const fetchUniversities = async (
   page: number,
